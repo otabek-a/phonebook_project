23e235d820df4738f7022f5b4b20a0bf78e57204
otabek-a <otabekabdurasulov245@gmail.com>
2024-11-12 16:31:58 +0500

    i have done it

--- contact.py
+++ contact.py
@@ -1,8 +1,23 @@
 class Contact:
-    first_name: str
-    last_name: str
-    phone_number: str
+   def __init__(self,first_name:str,last_name:str,phone_number:str):
+      self.first_name=first_name
+      self.last_name=last_name
+      self.phone_number=phone_number
+   def check_name(self):
+      if self.first_name[0].isupper() and self.last_name[0].isupper():
+         return True
+      return False
+   def check_phone_number(self,name=False):
+      self.name=name
+
+      indeks=len('+998 93 123 45 67')
+      if len(self.name)==indeks:
+         return True
+      return False
+   
 
 
-a = Contact()
-print(a)
+      
+      
+
+
