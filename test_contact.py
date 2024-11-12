e1671dc9ea34fcb024e62c2be0c1d420ac9622eb
m1 <uzbzarif@gmail.com>
2024-11-12 16:00:34 +0500

    add check test

--- test_contact.py
+++ test_contact.py
@@ -1,8 +1,20 @@
 from contact import Contact
 
-contact = Contact('Zarif','Naxalov','+998 93 123 45 67')
+
 
 def test_contact():
+    contact = Contact('Zarif','Naxalov','+998 93 123 45 67')
     assert contact.first_name == 'Zarif'
     assert contact.last_name == 'Naxalov'
     assert contact.phone_number == '+998 93 123 45 67'
+
+def test_check_phone_number():
+    contact = Contact('Zarif','Naxalov','+998 93 123 45 67')
+    assert contact.check_phone_number('+998 93 123 45 67')==True,'Phone number is not valid'
+    assert contact.check_phone_number('+998 93 123 45 6')==False,'Phone number is not valid'
+
+def test_check_name():
+    contact = Contact('Zarif','Naxalov','+998 93 123 45 67')
+    assert contact.check_name() == True
+    contact = Contact('zarif','Naxalov','+998 93 123 45 67')
+    assert contact.check_name() == False
